a49c52d50b37c785888065b3bab542bf94b83ced
SuperMistral <mediamart37@gmail.com>
2021-06-21 02:12:35 +0300

    Models to interact with articles

--- website/articles/admin.py
+++ website/articles/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Singer)
+class SingerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TextBlockClass)
+class TextBlockClassAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TextBlock)
+class TextBlockAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    pass
--- website/articles/models.py
+++ website/articles/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Singer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+
+class Song(models.Model):
+    singer = models.ForeignKey(Singer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    date_release = models.DateField()
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+
+# Классы блоков для css: center, left, right
+class TextBlockClass(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class TextBlock(models.Model):
+    text = models.TextField()
+    image = models.FileField(upload_to='text_blocks/', blank=True)
+    text_class = models.ForeignKey(TextBlockClass, on_delete=models.SET_DEFAULT, default="center")
+
+
+class Article(models.Model):
+    songs = models.ManyToManyField(Song)
+    section = models.ForeignKey(Section, on_delete=models.SET_NULL, null=True)
+    text_blocks = models.ManyToManyField(TextBlock)
+    date_release = models.DateTimeField()
+    date_change = models.DateTimeField()
